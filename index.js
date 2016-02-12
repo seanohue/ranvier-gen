e3ce040adba58a87156193749a5c1e44d4cc2213
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-02-11 23:21:56 -0500

    Add initial questions and filter for valid numbers.

--- index.js
+++ index.js
@@ -17,7 +17,34 @@ const Room = function roomConstructor(title, location, description, exits, area)
     this.area = area;
 };
 
-var map = [];
+init();
+
+function init() {
+    var howManyRooms = {
+        name: 'amount',
+        message: 'How many rooms would you like to create in this area?',
+        default: 1,
+        validate: validNumber,
+        filter: Number
+    }
+    var startingLocation = {
+        name: 'start',
+        message: 'What is the location # (vnum) you would like these rooms to start with?',
+        default: 1,
+        validate: validNumber,
+        filter: Number
+    }
+    inquirer.prompt([howManyRooms, startingLocation], beginRoomCreation);
+}
+
+function beginRoomCreation(answers) {
+    console.log(answers);
+}
+
+function validNumber(n){
+    n = Number(n);
+    return n.isInteger() && n > 0 || "Provide an integer.";
+}
 
 function en(string) {
     return {
