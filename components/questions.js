2a314f66bfca2a38a2fa2d190e903d3268333657
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-02-13 19:20:31 -0500

    Add questions for exit creation station.

--- components/questions.js
+++ components/questions.js
@@ -3,6 +3,10 @@ const validators = require(moduleDir + 'validators.js');
 const filters = require(moduleDir + 'filters.js')
 const templates = require(moduleDir + 'templates.js');
 
+/*
+///// Area Questions
+*/
+
 var howManyRooms = {
   name: 'amount',
   message: 'How many rooms would you like to create in this area?',
@@ -43,6 +47,10 @@ var areaName = {
   filter: filters.stringify
 };
 
+/*
+///// Room Questions
+*/
+
 var titleRoom = {
   name: 'title',
   message: 'What is the (player-friendly) title of this room?',
@@ -60,11 +68,31 @@ var describeRoom = {
 };
 
 var amountOfExits = {
-	name: 'numExits',
-	message: 'How many exits will this room have? Maximum of 6.',
-	default: 1,
-	validate: validators.between(1, 6),
-	filter: Number
+  name: 'numExits',
+  message: 'How many exits will this room have? Maximum of 6.',
+  default: 1,
+  validate: validators.between(1, 6),
+  filter: Number
+};
+
+/*
+///// Exit Questions
+*/
+
+var exitDestination = {
+  name: 'destination',
+  message: 'Which other room will this exit connect to? Enter a valid location number.',
+  default: 1,
+  validate: validators.positiveInt,
+  filter: Number
+};
+
+var exitLabel = {
+  name: 'label',
+  message: 'What command will the player type for this exit?',
+  default: 'out',
+  validate: validators.title,
+  filter: filters.stringify
 };
 
 module.exports = {
