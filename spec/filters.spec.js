58d1306dcfac13c3ea0e7c024b584b2afd186838
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-02-17 14:57:53 -0500

    Add test for leavemsg oh btw it is fixed :japanese_goblin:

--- spec/filters.spec.js
+++ spec/filters.spec.js
@@ -2,19 +2,34 @@ var should = require('chai').should();
 var filters = require('../components/filters.js');
 
 describe('>>> filter:', function() {
+
   describe('= stringify = ', function() {
+
     it('should return a string when input is a number',
       function() {
         filters.stringify(1234).should.equal('1234');
       });
+
     it('should return a string when input is an array',
       function() {
         filters.stringify(["1234", "5467"]).should.equal(
           '1234,5467');
       });
-    it('should return a trimmed string if there is leading/trailing whitespace',
+
+    it(
+      'should return a trimmed string if there is leading/trailing whitespace',
       function() {
-        filters.stringify('    1234   ').should.equal('1234');
+        filters.stringify('    1234   ').should.equal(
+          '1234');
       });
   });
+
+  describe('$ leave message $ ', function() {
+    it('should return a string with leading space',
+      function() {
+        filters.leaveMsg('burrito').should.equal(
+          ' burrito');
+      });
+  });
+
 });
