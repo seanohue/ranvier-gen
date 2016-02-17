bc89fb8067fffa47f173645e1542e0e860d1e7ba
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-02-17 16:51:47 -0500

    Add en test and change anon function to arrow function :heavy_plus_sign:

--- spec/filters.spec.js
+++ spec/filters.spec.js
@@ -1,60 +1,70 @@
 var should = require('chai').should();
 var filters = require('../components/filters.js');
 
-describe('>>> filter:', function() {
+describe('>>> filter:', () => {
 
-  describe('= stringify = ', function() {
+  describe('= stringify = ', () => {
 
     it('should return a string when input is a number',
-      function() {
+      () => {
         filters.stringify(1234).should.equal('1234');
       });
 
     it('should return a string when input is an array',
-      function() {
+      () => {
         filters.stringify(["1234", "5467"]).should.equal(
           '1234,5467');
       });
 
-    it('should return a trimmed string if there is leading/trailing whitespace',
-      function() {
+    it(
+      'should return a trimmed string if there is leading/trailing whitespace',
+      () => {
         filters.stringify('    1234   ').should.equal(
           '1234');
       });
   });
 
-  describe('$ leave message $ ', function() {
+  describe('$ leave message $ ', () => {
     it('should return a string with leading space',
-      function() {
+      () => {
         filters.leaveMsg('burrito').should.equal(
           ' burrito');
       });
-    it('should return a string with leading space when input is an array',
-      function() {
+    it(
+      'should return a string with leading space when input is an array',
+      () => {
         filters.leaveMsg(["1234", "5467"]).should.equal(
           ' 1234,5467');
       });
-    it('should return a string with leading space when input is an number',
-      function() {
+    it(
+      'should return a string with leading space when input is an number',
+      () => {
         filters.leaveMsg([1234]).should.equal(
           ' 1234');
       });
   });
 
-  describe('@ filename @', function() {
+  describe('@ filename @', () => {
     it('should return a lowercased string with no spaces',
-      function() {
+      () => {
         filters.filename('Dungeon Land').should.equal(
           'dungeonland');
       });
   });
 
-  describe('X no special chars X', function() {
+  describe('X no special chars X', () => {
     it('should return a string with special chars removed',
-      function() {
-        filters.noSpecialChars('bob\'s magical$%^ helicopter/ pants\\').should.equal(
+      () => {
+        filters.noSpecialChars(
+          'bob\'s magical$%^ helicopter/ pants\\').should.equal(
           'bobs magical helicopter pants');
       });
+  });
+
+  describe('%%% en %%%', () => {
+    it('should wrap string in an object with key of en', () => {
+      filters.en('ham').should.eql({ en: 'ham' });
+    });
   });
 
 });
