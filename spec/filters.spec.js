afcf013604080bc5f5832082519bbe337f6e4295
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-02-17 15:13:55 -0500

    Add tests for leaveMsg, filename, no special chars. :meat_on_bone:

--- spec/filters.spec.js
+++ spec/filters.spec.js
@@ -16,8 +16,7 @@ describe('>>> filter:', function() {
           '1234,5467');
       });
 
-    it(
-      'should return a trimmed string if there is leading/trailing whitespace',
+    it('should return a trimmed string if there is leading/trailing whitespace',
       function() {
         filters.stringify('    1234   ').should.equal(
           '1234');
@@ -29,6 +28,32 @@ describe('>>> filter:', function() {
       function() {
         filters.leaveMsg('burrito').should.equal(
           ' burrito');
+      });
+    it('should return a string with leading space when input is an array',
+      function() {
+        filters.leaveMsg(["1234", "5467"]).should.equal(
+          ' 1234,5467');
+      });
+    it('should return a string with leading space when input is an number',
+      function() {
+        filters.leaveMsg([1234]).should.equal(
+          ' 1234');
+      });
+  });
+
+  describe('@ filename @', function() {
+    it('should return a lowercased string with no spaces',
+      function() {
+        filters.filename('Dungeon Land').should.equal(
+          'dungeonland');
+      });
+  });
+
+  describe('X no special chars X', function() {
+    it('should return a string with special chars removed',
+      function() {
+        filters.noSpecialChars('bob\'s magical$%^ helicopter/ pants\\').should.equal(
+          'bobs magical helicopter pants');
       });
   });
 
